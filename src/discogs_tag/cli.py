8875179ff91f6d17d24c7dfaf3bbc83599ff552e
infojunkie <karim.ratib@gmail.com>
2023-09-20 16:21:47 -0700

    Support composers and dry run

--- src/discogs_tag/cli.py
+++ src/discogs_tag/cli.py
@@ -3,10 +3,11 @@ import mutagen
 import urllib.request
 import json
 import os
+from pprint import pprint
 from glob import glob
 from discogs_tag import __NAME__, __VERSION__
 
-def tag(release, dir = './'):
+def tag(release, dir = './', dry = False):
   """Tag the audio files in dir with the given Discogs release."""
   request = urllib.request.Request(f'https://api.discogs.com/releases/{release}', headers = {
     'User-Agent': f'{__NAME__} {__VERSION__}'
@@ -19,10 +20,21 @@ def tag(release, dir = './'):
     for n, track in enumerate(data['tracklist']):
       audio = mutagen.File(files[n], easy=True)
       audio['title'] = track['title']
-      audio['artist'] = ', '.join([artist['name'] for artist in track['artists']]) if 'artists' in track else ''
+      audio['artist'] = ', '.join([artist_name(artist) for artist in track['artists']]) if 'artists' in track else ''
       audio['tracknumber'] = track['position']
-      audio.save()
-    print(f'Processed {len(files)} audio files.')
+      composers = ', '.join([artist_name(composer) for composer in filter(lambda a: a['role'].casefold() == 'Written-By'.casefold(), track['extraartists'])]) if 'extraartists' in track else None
+      if (composers):
+        audio['composer'] = composers
+      if (dry):
+        pprint(audio)
+      else:
+        audio.save()
+
+    if (not dry):
+      print(f'Processed {len(files)} audio files.')
+
+def artist_name(artist):
+  return artist['anv'] or artist['name']
 
 def cli():
   fire.Fire(tag)
